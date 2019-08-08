9e744d163a7610d9628756e9274b13eae7e84a81
SalvadorKano <sgonzalez@mediomelon.mx>
2019-08-08 14:37:13 -0500

    Imagen en busqueda

---
+++ Series/Extension/ImageViewExtension.swift
@@ -0,0 +1,24 @@
+//
+//  ImageViewExtension.swift
+//  Series
+//
+//  Created by SalvadorKano on 8/8/19.
+//  Copyright © 2019 SalvadorKano. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
